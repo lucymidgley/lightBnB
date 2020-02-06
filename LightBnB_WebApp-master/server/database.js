da3cb776205af88b401ada174e5c5af046d45f60
Lucy <lucymidgley13@gmail.com>
2020-02-06 20:36:27 +0000

    Changed add add property function to allow users to add a listing to the app

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -96,7 +96,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, AVG(property_reviews.rating) as average_rating
   FROM properties 
-  JOIN property_reviews ON properties.id = property_reviews.property_id
+  LEFT JOIN property_reviews ON properties.id = property_reviews.property_id
   `;
   if(options.city) {
     queryParams.push(`%${options.city}%`)
@@ -117,10 +117,10 @@ const getAllProperties = function(options, limit = 10) {
     queryString += `GROUP BY properties.id
     HAVING AVG(property_reviews.rating) >= $${queryParams.length} `;
   } else {
-    queryParams.push(limit)
+    queryParams.push(5)
     queryString += `GROUP BY properties.id
     ORDER BY cost_per_night
-    LIMIT $${queryParams.length};
+    LIMIT $${queryParams.length} ;
     `;
   }
   
@@ -143,9 +143,14 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
+  const propertyValues = [property['owner_id'], property['title'], property['description'], property['thumbnail_photo_url'], property['cover_photo_url'], property['cost_per_night'], property['street'], property['city'], property['province'], property['post_code'], property['country'], property['parking_spaces'], property['number_of_bathrooms'], property['number_of_bedrooms']];
+  console.log(propertyValues);
+  return pool.query(`
+  INSERT INTO properties (
+    owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms, active) 
+    VALUES (
+    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14, true)
+    RETURNING *;`, propertyValues);
 }
 exports.addProperty = addProperty;
+// (owner_id,title, description, thumbnail_photo_url,  cover_photo_url,  cost_per_night,  street, city, province, post_code , country,parking_spaces, number_of_bathrooms, number_of_bedrooms, active) 
